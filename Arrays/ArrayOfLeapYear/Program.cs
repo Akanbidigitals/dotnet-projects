15e6b0f56c9a64d0615d817eabc3ec5502e4445a
Munirudeen Akanbi <adsdesigntech@gmail.com>
2024-05-12 20:43:43 +0100

    Leap year Array task submitted

---
+++ Arrays/ArrayOfLeapYear/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using ArrayOfLeapYear.models;
+
+int[] years = { 1995, 1997, 2005, 2010,2014,2020, 2011, 2024,2028 };
+
+var calculate = new CalculateLeapYear();
+
+calculate.GetLeapyear(years);
---
+++ Arrays/ArrayOfLeapYear/models/CalculateLeapYear.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayOfLeapYear.models
+{
+    public class CalculateLeapYear
+    {
+        public void GetLeapyear (int[] years)
+        {
+            Console.WriteLine("Leap years");
+            int leapYearCounter = 0;
+            foreach (int year in years)
+            {
+                if(year <= DateTime.Now.Year)
+                {
+                    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0 )
+                    {
+                        Console.WriteLine($"-------------------------{year} is a leap year------------------------");
+                        leapYearCounter++;
+                    }
+                         Console.WriteLine($"{year} is in Century : {CalculateLeapYear.GetCentury(year)}th.");
+
+                }
+                else
+                {
+                Console.WriteLine($"{year} is a future year cant be checked");
+
+                }
+            }
+                         Console.WriteLine($"-------------Number of leap years in the Array = {leapYearCounter}-----------------------");
+        }
+       
+        static int GetCentury (int year)
+        {
+            int century = year / 100;
+            return century;
+        }
+    }
+}
