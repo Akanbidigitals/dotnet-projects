874d9fb81fec8761857cdb9fdda068bc58f53682
Munirudeen Akanbi <adsdesigntech@gmail.com>
2024-05-28 23:29:40 +0100

    Rveerse long words algorithm

---
+++ ReverseWordMoreThan5Letters/Model/Start.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseWordMoreThan5Letters.Model
+{
+    internal class Start
+    {
+        public string ReverseWordWith5Letters(string input)
+        {
+            string[] splitInput = input.Split(' ');
+            for(int i = 0; i < splitInput.Length; i++)
+            {
+                if (splitInput[i].Length >= 5)
+                {
+                    char[] charArray = splitInput[i].ToCharArray();
+                    Array.Reverse(charArray);
+                    splitInput[i] = new string(charArray);
+                } 
+            }
+            string result = string.Join(" ", splitInput);
+            return result;
+
+        }
+    }
+}
---
+++ ReverseWordMoreThan5Letters/Program.cs
@@ -0,0 +1,18 @@
+﻿using ReverseWordMoreThan5Letters.Model;
+
+
+Console.WriteLine("Welcome to Reverse 5 letters above words");
+
+Console.WriteLine("-----------------------------------------");
+
+Console.WriteLine("Pls enter your words");
+
+string input = Console.ReadLine();
+
+var validate = new Start();
+
+
+var result = validate.ReverseWordWith5Letters(input);
+
+Console.WriteLine(result);
+
