3a246b13ad95ce2415fb94a63eb215fecfba3945
Munirudeen Akanbi <adsdesigntech@gmail.com>
2024-05-28 23:53:07 +0100

    Sorting Array task

---
+++ SortingArrays/Models/Start.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingArrays.Models
+{
+    internal class Start
+    {
+        public int[] SortArrays(int[] arr)
+        {
+
+            Array.Sort(arr);
+            return arr;
+        }
+    }
+}
---
+++ SortingArrays/Program.cs
@@ -0,0 +1,14 @@
+﻿using SortingArrays.Models;
+
+int[] input = { 5, 6, 8, 0, 10, 15, 32, 78, 23 };
+
+var sortArr = new Start();
+
+int[] result = sortArr.SortArrays(input);
+
+    Console.WriteLine("Ascending Order");
+foreach (int i in result)
+{
+
+    Console.WriteLine($"num : {i}");
+}
